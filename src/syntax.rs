ff06fe7a90ec79223b5180bd43d518db3d1a19a0
hubert <hubert@hubert.com>
2024-03-05 06:59:00 +0100

    save syntaxe

--- src/syntax.rs
+++ src/syntax.rs
@@ -4,6 +4,7 @@ use std::{ffi::OsStr, future::Future, process::ExitStatus};
 use tokio::process::Command;
 
 use crate::process::{new_process, ProcessStream};
+use tokio::io::AsyncWriteExt;
 
 pub struct Cmd(Command);
 
@@ -14,8 +15,36 @@ impl<T: AsRef<OsStr>> From<T> for Cmd {
     }
 }
 
-pub fn run(cmd: Cmd) {}
-pub fn apply(cmd: Cmd, stdin: &[u8]) {}
+pub async fn run<C: Into<Cmd>>(cmd: C) -> std::io::Result<ExitStatus> {
+    let mut command: Command = cmd.into().0;
+    command
+        .stdin(std::process::Stdio::inherit())
+        .stdout(std::process::Stdio::inherit())
+        .stderr(std::process::Stdio::inherit())
+        .status()
+        .await
+}
+
+pub async fn apply<C: Into<Cmd>, U: AsRef<[u8]> + std::marker::Send + 'static>(
+    cmd: C,
+    input: U,
+) -> std::io::Result<std::process::Output> {
+    let mut command: Command = cmd.into().0;
+    let mut child = command
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
+        .stderr(std::process::Stdio::piped())
+        .spawn()?;
+    let mut stdin = child.stdin.take().unwrap();
+    tokio::spawn(async move {
+        match stdin.write_all(input.as_ref()).await {
+            Ok(()) => {}
+            Err(_) => {} // cas où le process n'a pas lu toute son entrée
+        }
+    });
+    child.wait_with_output().await
+}
+
 pub fn stream<I: TryStream<Ok = Bytes>, C: Into<Cmd>>(
     cmd: C,
     stdin: I,
